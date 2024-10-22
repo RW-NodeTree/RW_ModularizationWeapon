5713a8652e100ed5758c8249f4a979b1a0d2973b
Future <1609605489@qq.com>
2024-10-22 23:24:49 +0800

    build release

--- BuildRelease.sh
+++ BuildRelease.sh
@@ -3,15 +3,15 @@
 dotnet build -c ReleaseV13
 dotnet build -c ReleaseV14
 dotnet build -c ReleaseV15
-if [ -f ./1.3/Assemblies/RW_NodeTree.pdb ]
+if [ -f ./1.3/Assemblies/RW_ModularizationWeapon.pdb ]
 then
-    rm -f ./1.3/Assemblies/RW_NodeTree.pdb
+    rm -f ./1.3/Assemblies/RW_ModularizationWeapon.pdb
 fi
-if [ -f ./1.4/Assemblies/RW_NodeTree.pdb ]
+if [ -f ./1.4/Assemblies/RW_ModularizationWeapon.pdb ]
 then
-    rm -f ./1.4/Assemblies/RW_NodeTree.pdb
+    rm -f ./1.4/Assemblies/RW_ModularizationWeapon.pdb
 fi
-if [ -f ./1.5/Assemblies/RW_NodeTree.pdb ]
+if [ -f ./1.5/Assemblies/RW_ModularizationWeapon.pdb ]
 then
-    rm -f ./1.5/Assemblies/RW_NodeTree.pdb
+    rm -f ./1.5/Assemblies/RW_ModularizationWeapon.pdb
 fi
